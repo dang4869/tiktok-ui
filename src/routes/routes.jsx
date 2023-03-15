19bd5857701fd04b00699f900c8383818d0de586
Dang <dangch@compedia.vn>
2023-03-15 21:41:09 +0700

    sidebar ui#1

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import config from '~/config';
 import { HeaderOnly } from '~/layouts';
 import Following from '~/pages/Following';
 import Home from '~/pages/Home';
+import Live from '~/pages/Live';
 import Profile from '~/pages/profile';
 import Search from '~/pages/Search';
 import Upload from '~/pages/Upload';
@@ -12,6 +13,7 @@ const publicRoutes = [
     { path: config.routers.profile, component: Profile },
     { path: config.routers.upload, component: Upload, layout: HeaderOnly },
     { path: config.routers.search, component: Search, layout: null },
+    { path: config.routers.live, component: Live },
 ];
 const privateRoutes = [];
 
